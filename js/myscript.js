9fe21212c0cd3a9074cd1f8c01a4acc69e99089b
pri-ake <chiran.cha.no.1@gmail.com>
2016-01-14 06:25:50 +0000

    Angular-1.4.8対応したよ

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,13 @@
-var mainCtrl = function($scope) {
-  $scope.users = [
-    {"name":"taguchi", "score":52.22},
-    {"name":"tanaka", "score":38.22},
-    {"name":"yamada", "score":11.11},
-    {"name":"himeda", "score":100.100},
-    {"name":"iino", "score":60.22},
-    {"name":"ohara", "score":22.22},
-    {"name":"shimoda", "score":90.11},
-    {"name":"kakei", "score":40.100},
-  ];
-  $scope.today = new Date();
-}
+var myApp = angular.module("myApp", []);
 
-var userItemCtrl = function($scope) {
-  $scope.increment = function() {
-    $scope.user.score++;  // score += 1
+myApp.controller('mainCtrl', function ($scope){
+  $scope.addUser = function() {
+    // ユーザーを追加する処理を書くよ
   }
-  $scope.decrement = function() {
-    $scope.user.score--;  // score -= 1
-  }
-}
+  $scope.user = {
+    "adult":"adult",
+    "phone":"Android",
+    "color":"green"
+  };
+  $scope.colors = ['red', 'blue', 'pink','yellow', 'green'];
+});
